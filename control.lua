ab886864cf4bccbe660aea56fd22e9c8c79e15d0
Braxbro <63511112+Braxbro@users.noreply.github.com>
2021-12-16 11:55:29 -0500

    Update control.lua

--- control.lua
+++ control.lua
@@ -1,10 +1,10 @@
 local function getMaximumEnergyOfRecipe(productName)
 	local recipes
 	if game.item_prototypes[name] then
-		recipes = game.get_filtered_recipe_prototypes({{filter = "has-product-item", elem_filters = {{filter = "name", name = productName) --recipe is an item
+		recipes = game.get_filtered_recipe_prototypes({{filter = "has-product-item", elem_filters = {{filter = "name", name = productName}}}}) --recipe is an item
 	else 
 		if game.fluid_prototypes[name] then
-			recipes = game.get_filtered_recipe_prototypes({{filter = "has-product-fluid", elem_filters = {{filter = "name", name = productName) --recipe is a fluid
+			recipes = game.get_filtered_recipe_prototypes({{filter = "has-product-fluid", elem_filters = {{filter = "name", name = productName}}}}) --recipe is a fluid
 		else
 			return 0 --cannot find recipe
 		end
@@ -28,20 +28,21 @@ local function getMaximumEnergyOfRecipe(productName)
 		for _, ingredient in pairs do
 			maxProductEnergy = maxProductEnergy + getMaximumEnergyOfRecipe(ingredient.name) * ingredient.amount
 		end
-		return maxProductEnergy
-	else
-		return 0
 	end
+	return maxProductEnergy
 end
 	
 
 local function calculateScienceCosts()
-	local tools = game.get_filtered_item_prototypes({{filter = "tool"}})
-	for _, pack in pairs(tools) do
-		
-	
+	local packs = game.get_filtered_item_prototypes({{filter = "subgroup", subgroup = "science-pack"}})
+	for _, pack in pairs(packs) do
+		global.packCost[pack.name] = getMaximumEnergyOfRecipe(pack.name)
+	end
+end
 
 local function onInit()
+	global.packCost = {}
+	global.storedCost = {}
     if settings.startup["startup-unlock-tech"].value then
 	   for index, force in pairs(game.forces) do
 	   force.technologies["automation"].researched = true
@@ -51,6 +52,7 @@ local function onInit()
 	   force.technologies["logistics"].researched = true
 	   end
 	end
+	calculateScienceCosts()
 end
 
 function onDeathHandler(event)
@@ -60,8 +62,15 @@ function onDeathHandler(event)
 	if (attackingForce == nil) then
 		return
 	end
-	if(attackingForce.current_research == nil) then
-		return
+	local overflow = settings.startup["science-overflow-mode"].value
+	local costPerDamage = settings.startup["cost-per-damage"].value
+	if (attackingForce.current_research == nil) then
+		if overflow = "void" then -- if not retaining science
+			return
+		else
+			global.storedCost[attackingForce.name] = (global.storedCost[attackingForce.name] or 0) + (entity.prototype.max_health * costPerDamage) * (1 + attackingForce.laboratory_productivity_bonus) -- if retaining science (keep or decay modes)
+			return
+		end
 	end
 
 	-- Do not reward destroying your allied or neutral entities (like trees)
@@ -69,29 +78,46 @@ function onDeathHandler(event)
 		return
 	end
 	
-	-- Disable reward for kills on cease-fired targets if not enabled 
-	if ((not settings.startup["reward-neutral-kills"]) and attackingForce.get_cease_fire(entity.force)) then
+	-- Disable reward for kills on cease-fired/neutral targets if not enabled 
+	if ((not settings.startup["reward-neutral-kills"].value) and attackingForce.get_cease_fire(entity.force)) then
 		return
 	end
 
-	local researchIngredientCount = 0
+	local researchUnitCost
 	for index, ingredient in pairs(attackingForce.current_research.research_unit_ingredients) do
-		researchIngredientCount = researchIngredientCount + ingredient.amount
+		researchUnitCost = researchUnitCost + global.packCost[ingredient.name] * ingredient.amount
 	end
-	local researchUnitCost = researchIngredientCount * (attackingForce.current_research.research_unit_energy / 60 / (1 + attackingForce.laboratory_speed_modifier))
+	researchUnitCost = (researchUnitCost + attackingForce.current_research.research_unit_energy) / (1 + attackingForce.laboratory_speed_modifier))
 	local researchTotalCost = researchUnitCost * attackingForce.current_research.research_unit_count
-	-- TODO: rename
-	local damageEffectScale = settings.startup["damage-effect-scale"].value / 100
-	local researchDelta = entity.prototype.max_health / (researchTotalCost * damageEffectScale) * (1 + attackingForce.laboratory_productivity_bonus)
+	local researchDelta = (entity.prototype.max_health * costPerDamage) / researchTotalCost * (1 + attackingForce.laboratory_productivity_bonus)
 	local researchProgress = attackingForce.research_progress + researchDelta
-	if(researchProgress >= 1)then
+	if (researchProgress >= 1) then
 		attackingForce.research_progress = 0
 		attackingForce.current_research.researched = true
 	else
 		attackingForce.research_progress = researchProgress
 	end
-
 end
 
+local function onTick()
+	for _, force in pairs(game.forces) do
+		if (force.current_research and global.storedCost[force.name]) then
+			local researchUnitCost
+			for index, ingredient in pairs(force.current_research.research_unit_ingredients) do
+				researchUnitCost = researchUnitCost + global.packCost[ingredient.name] * ingredient.amount
+			end
+			researchUnitCost = (researchUnitCost + force.current_research.research_unit_energy) / (1 + force.laboratory_speed_modifier))
+			local researchTotalCost = researchUnitCost * force.current_research.research_unit_count
+			local researchDelta = global.storedCost[force.name]/researchTotalCost
+			local researchProgress = force.research_progress + researchDelta
+			global.storedCost[force.name] = math.max(0, global.storedCost[force.name] - (researchTotalCost * (1 - force.research_progress))) -- spend any stored science
+		else if settings.startup["science-overflow-mode"].value = "decay" then -- don't decay stored science that was spent this tick
+			global.storedCost[force.name] = (1 - settings.startup["science-decay-per-tick"]) * global.storedCost[force.name]
+		end
+	end
+end
+		
 script.on_init(onInit)
+script.on_tick(onTick)
+script.on_configuration_changed(calculateScienceCosts)
 script.on_event(defines.events.on_entity_died, onDeathHandler)
