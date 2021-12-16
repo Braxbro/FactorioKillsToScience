0e337eabaaf34e2210114c6fc39ba087bbc4649c
Braxbro <63511112+Braxbro@users.noreply.github.com>
2021-12-16 13:28:42 -0500

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -8,9 +8,9 @@ data:extend({
 })
 data:extend({
 		{
-			type = "bool-setting"
-			name = "reward-neutral-kills"
-			setting_type = "startup"
+			type = "bool-setting",
+			name = "reward-neutral-kills",
+			setting_type = "startup",
 			default_value = false
 		} 
 })
@@ -19,16 +19,16 @@ data:extend({
 			type = "double-setting",
 			name = "cost-per-damage",
 			setting_type = "startup",
-			minimum_value = 0.01
-			default_value = 1
+			minimum_value = 0.01,
+			default_value = 1,
 			maximum_value = 100
 		}
 })
 data:extend({
 		{
-			type = "string-setting"
-			name = "science-overflow-mode"
-			setting_type = "startup"
+			type = "string-setting",
+			name = "science-overflow-mode",
+			setting_type = "startup",
 			allowed_values = {"void", "keep", "decay"}
 		}
 })
@@ -37,8 +37,8 @@ data:extend({
 			type = "double-setting",
 			name = "science-decay-per-tick",
 			setting_type = "startup",
-			minimum_value = 0.01
-			default_value = 0.05
+			minimum_value = 0.01,
+			default_value = 0.05,
 			maximum_value = 0.99
 		}
 })
