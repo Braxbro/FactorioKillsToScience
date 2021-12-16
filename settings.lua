82ef181730903234c8dcfde702becb6eee1c12bc
Braxbro <63511112+Braxbro@users.noreply.github.com>
2021-12-16 07:48:52 -0500

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -1,10 +1,18 @@
 data:extend({
     {
-        type = "bool-setting",
-        name = "startup-unlock-tech",
-        setting_type = "startup",
-        default_value = true
+      type = "bool-setting",
+      name = "startup-unlock-tech",
+      setting_type = "startup",
+      default_value = true
     }
+})
+data:extend({
+    {
+      type = "bool-setting"
+      name = "reward-neutral-kills"
+      setting_type = "startup"
+      default_value = false
+    } 
 })
 data:extend({
     {
