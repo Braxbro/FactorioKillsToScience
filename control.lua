de38af4901556aba731ea396ee7b6e01b2161510
Braxbro <63511112+Braxbro@users.noreply.github.com>
2021-12-16 23:08:16 -0500

    Update control.lua

--- control.lua
+++ control.lua
@@ -37,7 +37,7 @@ end
 local function calculateScienceCosts()
 	local packs = game.get_filtered_item_prototypes({{filter = "subgroup", subgroup = "science-pack"}})
 	for _, pack in pairs(packs) do
-		global.packCost[pack.name] = math.floor(getMaximumEnergyOfRecipe(pack.name, 0)) + 1
+		global.packCost[pack.name] = math.floor(getMaximumEnergyOfRecipe(pack.name, 0) + .5)
 	end
 end
 
